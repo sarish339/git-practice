7a2d4912bb8ccc504def6c446e6f36100ce8f135
sarish339 <sarishsunkara339@gmail.com>
2025-11-04 12:18:06 +0530

    added conversation script

---
+++ 02-converstation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi Suresh, How are you?"
+echo "Suresh:: Hello Ramesh. Iam fine. How are you?"
+echo " Ramesh:: I am doing good suresh. What's going on?"
+echo "Suresh:: I started learding Shell Script Ramesh"
