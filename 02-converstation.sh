c58bc093e6076ba28fe615d3df07b30ea4b85023
sarish339 <sarishsunkara339@gmail.com>
2025-11-04 12:23:49 +0530

    Spaces cleared

--- 02-converstation.sh
+++ 02-converstation.sh
@@ -2,5 +2,5 @@
 
 echo "Ramesh:: Hi Suresh, How are you?"
 echo "Suresh:: Hello Ramesh. Iam fine. How are you?"
-echo " Ramesh:: I am doing good suresh. What's going on?"
-echo "Suresh:: I started learding Shell Script Ramesh"
+echo "Ramesh:: I am doing good suresh. What's going on?"
+echo "Suresh:: I started learning Shell Script Ramesh"
