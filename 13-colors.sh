9319cedc2b7ad847c86a5d6c36b1a4ded4df2131
sarish339 <sarishsunkara339@gmail.com>
2025-11-06 11:15:23 +0530

    variable

---
+++ 13-colors.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "Please run this script with root priveleges"
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+then
+    echo "$2 is $R failed $N"
+        exit 1
+    else
+        echo "$2 is $G success $N"
+    fi  
+}
+
+CHECK_ROOT
+
+dnf list installed git
+
+
+if [ $? -ne 0 ]
+then
+    echo "Git is not installed, going to install.."
+    dnf install git -y
+    VALIDATE $? "Installing Git"
+else
+    echo "Git is already installed, nothing to do"
+fi   
+
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "MySQL is not installed, going to install"
+    dnf install mysql -y
+    VALIDATE $? "Installing MySQL"
+else
+    echo "MySQL is already installed, nothing to do"
+fi
