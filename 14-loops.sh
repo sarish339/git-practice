f35b69c3cde68bec0eab014515f43b9c62422aa0
sarish339 <sarishsunkara339@gmail.com>
2025-11-07 11:57:42 +0530

    shell

--- 14-loops.sh
+++ 14-loops.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
-# echo "1"
-# echo "2"
-# echo "3"
-# echo "4"
-# echo "5"
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
-for i in {0..1000}
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "Please run this script with root priveleges"
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+then
+    echo -e "$2 is $R failed $N"
+        exit 1
+    else
+        echo -e "$2 is $G success $N"
+    fi  
+}
+
+CHECK_ROOT
+
+# sh 15-loops.sh mysql git nginx...
+for package in $@  #$@ refers to all arguments passed to it
 do
-    echo $i
+    echo $package
 done
