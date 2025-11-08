349a9d9d944671c70a683a3e0733d426c5814db4
sarish339 <sarishsunkara339@gmail.com>
2025-11-08 16:20:16 +0530

    shell

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -23,10 +23,10 @@ CHECK_ROOT(){
 VALIDATE(){
     if [ $? -ne 0 ]
 then
-    echo -e "$2 is $R failed $N"  | tee -a $LOG_FILE
+    echo -e "$2 is $R failed $N" | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "$2 is $G success $N"  | tee -a $LOG_FILE
+        echo -e "$2 is $G success $N" | tee -a $LOG_FILE
     fi  
 }
 
