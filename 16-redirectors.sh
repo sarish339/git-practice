85c9762d712af7109bd8f3909028b04a7d1942b8
sarish339 <sarishsunkara339@gmail.com>
2025-11-08 15:46:43 +0530

    shell

---
+++ 16-redirectors.sh
@@ -0,0 +1,46 @@
+#!/bin/bsh
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo  -e "$R Please run this script with root priveleges $N" &>>$LOG_FILE
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+then
+    echo -e "$2 is $R failed $N" &>>$LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is $G success $N" &>>$LOG_FILE
+    fi  
+}
+
+CHECK_ROOT
+
+# sh 15-loops.sh mysql git nginx...
+# for package in $@  #$@ refers to all arguments passed to it
+# do
+#     dnf list installed $package
+#     if [ $? -ne 0 ]
+#     then
+#         echo "$package is not installed, going to install it.."
+#         dnf install $package -y
+#         VALIDATE $? "Installing $package"
+#     else
+#         echo "$package is already installed.. nothing to do"
+#     fi
+# done
