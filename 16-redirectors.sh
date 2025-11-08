90c14beab0c5ce843709c3283a86b2d6afb7cb5a
sarish339 <sarishsunkara339@gmail.com>
2025-11-08 15:50:17 +0530

    shell

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -10,6 +10,7 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
 
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
@@ -32,15 +33,15 @@ then
 CHECK_ROOT
 
 # sh 15-loops.sh mysql git nginx...
-# for package in $@  #$@ refers to all arguments passed to it
-# do
-#     dnf list installed $package
-#     if [ $? -ne 0 ]
-#     then
-#         echo "$package is not installed, going to install it.."
-#         dnf install $package -y
-#         VALIDATE $? "Installing $package"
-#     else
-#         echo "$package is already installed.. nothing to do"
-#     fi
-# done
+for package in $@  #$@ refers to all arguments passed to it
+do
+    dnf list installed $package &>>$LOG_FILE
+    if [ $? -ne 0 ]
+    then
+        echo "$package is not installed, going to install it.." &>>$LOG_FILE
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "Installing $package" &>>$LOG_FILE
+    else
+        echo "$package is already $Y installed.. nothing to do $N" &>>$LOG_FILE
+    fi
+done
