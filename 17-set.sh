cb67e427d8601fad391a52e5d76cff725c28a139
sarish339 <sarishsunkara339@gmail.com>
2025-11-10 12:09:25 +0530

    shell

---
+++ 17-set.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e # setting the automatic exit, if we get the error, set -ex for debug
+echo "Hello world success"
+echooo " Hello world failure"
+echo " Hello world after failure"
