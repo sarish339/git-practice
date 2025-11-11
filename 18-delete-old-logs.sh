5bd021e2e26476c3c80babc0e519723660893ee9
sarish339 <sarishsunkara339@gmail.com>
2025-11-11 11:08:44 +0530

    shell

---
+++ 18-delete-old-logs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SOURCE_DIR=/home/ec2-user/logs
+
+if  -d $SOURCE_DIR ]
+then
+    echo "$SOURCE_DIR Exists"
+else
+    echo "$SOURCE_DIR does not exist"
+    exit 1
+fi
+
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+echo "Files: $FILES"
+
+while IFS= read -r line #IFS = Internal file seperator, empty  it will ignore while space -r is not to ignore special charcaters like /
+do
+    echo "Deleting line: $line"
+    rm -rf $line
+done <<< $FILES
